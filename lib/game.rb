3dfc54d9b7e820a0df6db979a682d86aa99bc4eb
jee-el <98971420+Jee-El@users.noreply.github.com>
2022-09-08 21:57:31 +0100

    Fix: game not saving properly

--- lib/game.rb
+++ lib/game.rb
@@ -3,6 +3,8 @@
 require 'yaml'
 
 require_relative './board'
+require_relative './human_player'
+require_relative './computer_player'
 require_relative './display'
 
 module Hangman
@@ -10,76 +12,61 @@ module Hangman
   class Game
     include Display
 
-    attr_reader :guesser, :picker, :board, :words
+    attr_reader :guesser, :picker, :board, :words, :secret_word
 
-    def initialize(guesser, picker, word_length)
-      @saved_games = YAML.safe_load(
-        File.read('saved_games.yaml'),
-        permitted_classes: [Board, ComputerPlayer, Game, HumanPlayer]
-      ) || {}
-      @is_resuming_game = resume?
-      @is_resuming_game ? resume_game : new_game(guesser, picker, word_length)
+    def initialize(guesser, picker, words)
+      @guesser = guesser
+      @picker = picker
+      @board = Board.new(words.first.length)
+      @words = words
     end
 
     def start
       show_how_to_save_game
-      word = @picker.secret_word(@words) unless @is_resuming_game
-      save_game if save_game?(word)
+      @secret_word ||= @picker.secret_word(@words)
+      save_game if save_game?(@secret_word)
       puts(@board.slots) || puts
-      start_guessing(word)
+      start_guessing
     end
 
     private
 
-    def start_guessing(word)
+    def start_guessing
       until game_over?
         guess = @guesser.guess.downcase
         return save_game if save_game?(guess)
 
         @guesser.guesses_count += 1
         clear_screen if @guesser.guesses_count.even?
-        @board.update(guess, word)
+        @board.update(guess, @secret_word)
       end
-      game_over(word)
+      game_over
     end
 
-    def resume?
-      return if @saved_games.empty?
-
-      puts 'Do you wanna start a new game, or resume?'
-      puts "1 => resume\n 2 => new game\n"
-      answer = gets.chomp.downcase
-      return true if answer == '1'
-    end
-
-    def new_game(guesser, picker, word_length)
-      @saved_games[:saved_games_count] ||= 0
-      @guesser = guesser
-      @picker = picker
-      @board = Board.new(word_length)
-      @words = File.foreach('dictionary.txt').map(&:chomp).filter do |line|
-        line.length == word_length
-      end
-    end
-
-    def save_game?(word)
-      word == ':w'
+    def save_game?(input)
+      input == ':w'
     end
 
     def save_game
-      @saved_games[:saved_games_count] += 1
-      @saved_games["saved_game_#{@saved_games[:saved_games_count]}"] = self
-      File.write('saved_games.yaml', YAML.dump(@saved_games))
+      saved_games = YAML.safe_load(
+        File.read('saved_games.yaml'),
+        permitted_classes: [Hangman::Board, Hangman::Game, Hangman::HumanPlayer, Hangman::ComputerPlayer, Symbol]
+      ) || {}
+      saved_games[:saved_games_count] ||= 0
+      saved_games[:saved_games_count] += 1
+      index = saved_games[:saved_games_count]
+      saved_games[:"saved_game_#{index}"] = self
+      File.write('saved_games.yaml', YAML.dump(saved_games), mode: 'a')
     end
 
     def game_over?
       !@board.slots.include?('_') || @guesser.guesses_count == 7
     end
 
-    def game_over(word)
+    def game_over
       puts
       if @guesser.guesses_count == 7
-        announce_winner(@picker.name, word)
+        announce_winner(@picker.name)
       else
         announce_winner(@guesser.name)
       end
--- lib/main.rb
+++ lib/main.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require 'yaml'
+
 require_relative './platform_name'
 require_relative './display'
 require_relative './computer_game_setup'
@@ -8,18 +10,44 @@ require_relative './game'
 
 def play
   Hangman::Display.welcome
-  case Hangman::PlatformName.get
-  when 'phone' then game_setup = Hangman::PhoneGameSetup.new
-  when 'computer' then game_setup = Hangman::ComputerGameSetup.new
+  x = resume?
+  x ? resume_game(x) : new_game
+end
+
+def resume?
+  saved_games = YAML.safe_load(
+    File.read('saved_games.yaml'),
+    permitted_classes: [Hangman::Board, Hangman::Game, Hangman::HumanPlayer, Hangman::ComputerPlayer, Symbol]
+  ) || {}
+  return if saved_games.empty?
+
+  puts 'Do you wanna start a new game, or resume?'
+  puts "1 => resume\n\n2 => new game\n"
+  answer = gets.chomp.downcase
+  return saved_games[:saved_game_1] if answer == '1'
+end
+
+def resume_game(game)
+  game.start
+end
+
+def new_game
+  game_setup.run
+  players = game_setup.settings[:players]
+
+  words = File.foreach('dictionary.txt').map(&:chomp).filter do |word|
+    word.length == game_setup.settings[:word_length]
   end
 
-  game_setup.run
-
-  players = game_setup.settings[:players]
-  word_length = game_setup.settings[:word_length]
-
-  game = Hangman::Game.new(*players, word_length)
+  game = Hangman::Game.new(*players, words)
   game.start
+end
+
+def game_setup
+  case Hangman::PlatformName.get
+  when 'phone' then Hangman::PhoneGameSetup.new
+  when 'computer' then Hangman::ComputerGameSetup.new
+  end
 end
 
 play
