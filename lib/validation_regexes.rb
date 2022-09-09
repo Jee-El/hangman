f93e6957d76633cd43f1ef1d48fed098025157f9
jee-el <98971420+Jee-El@users.noreply.github.com>
2022-09-09 23:27:43 +0100

    Remove unnecessary module

--- lib/validation_regexes.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-module Hangman
-  module ValidationRegexes
-    WORD_LENGTH = { regex: /^\d{1}$/, error: 'Please enter a number <= 18 : ' }.freeze
-    MAX_GUESSES = { regex: /^\d{1}$/, error: 'Please enter a number : ' }.freeze
-    VALID_GUESS = { regex: /^[a-z]{1}$/i, error: 'Please enter a letter :' }.freeze
-  end
-end
