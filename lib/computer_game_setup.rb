ef9e785681def42371da128a0a74a4998ea0d0f2
jee-el <98971420+Jee-El@users.noreply.github.com>
2022-09-09 02:59:00 +0100

    Organize code by containerizing methods in modules

--- lib/computer_game_setup.rb
+++ lib/computer_game_setup.rb
@@ -3,24 +3,13 @@
 require 'tty-prompt'
 
 require_relative './game_setup'
+require_relative './computer_display'
 
 module Hangman
   # Setup for computer users
   # It uses some tty-prompt features which can't be used on phone
   # (e.g: replit on phone)
   class ComputerGameSetup < GameSetup
-    def clarify_rules
-      show_guide
-      @prompt.keypress('Press any key to continue')
-      clear_screen
-    end
-
-    def human_player_role
-      puts
-      nums_by_roles = { 'Word Guesser' => '1', 'Word Picker' => '2' }
-      role = @prompt.select('Choose your role :', ['Word Guesser', 'Word Picker'])
-      clear_screen
-      nums_by_roles[role]
-    end
+    include ComputerDisplay
   end
 end
--- lib/phone_game_setup.rb
+++ lib/phone_game_setup.rb
@@ -3,47 +3,13 @@
 require 'tty-box'
 
 require_relative './game_setup'
+require_relative './phone_display'
 
 module Hangman
   # Setup for phone users
   # It doesn't use some tty-prompt features that can't be used on phone
   # (e.g: replit on phone)
   class PhoneGameSetup < GameSetup
-    def clarify_rules
-      show_guide
-      loop do
-        puts 'Press return to continue'
-        input = gets.chomp
-        puts
-        break if input.empty?
-      end
-      clear_screen
-    end
-
-    def human_player_role
-      puts
-      ask_for_human_player_role
-      loop do
-        print 'Enter either the number 1 or 2 : '
-        role = gets.chomp
-        puts
-        break clear_screen || role if valid_role?(role)
-      end
-    end
-
-    def ask_for_human_player_role
-      roles_by_nums_str = "1 => Word Guesser\n\n2 => Word Picker\n"
-      puts
-      puts TTY::Box.frame(
-        roles_by_nums_str,
-        padding: [1, 1],
-        title: { top_center: ' Choose your role : ' }
-      )
-      puts
-    end
-
-    def valid_role?(role)
-      %w[1 2].include?(role)
-    end
+    include PhoneDisplay
   end
 end
