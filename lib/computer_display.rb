5c640169bbd0310312a45f6c2b035589ca522e87
jee-el <98971420+Jee-El@users.noreply.github.com>
2022-09-09 23:33:39 +0100

    Replace module methods with class methods

--- lib/computer_display.rb
+++ lib/computer_display.rb
@@ -3,28 +3,10 @@ require 'tty-prompt'
 module Hangman
   module Display
     module ComputerDisplay
-      SAVED_GAMES = YAML.safe_load(
-        File.read('saved_games.yaml'),
-        permitted_classes: [Board, Game, HumanPlayer, ComputerPlayer, Symbol, Time]
-      ) || {}
-
-      def self.saved_game_to_load
-        saved_game = TTY::Prompt.new.select('Choose a game :', SAVED_GAMES.keys[1..])
-        SAVED_GAMES[saved_game.to_sym]
-      end
-
       def clarify_rules
         show_guide
         @prompt.keypress('Press any key to continue')
         Display.clear
-      end
-
-      def human_player_role
-        puts
-        roles_to_nums = { 'Word Guesser' => '1', 'Word Picker' => '2' }
-        role = @prompt.select('Choose your role :', ['Word Guesser', 'Word Picker'])
-        Display.clear
-        roles_to_nums[role]
       end
     end
   end
--- lib/phone_display.rb
+++ lib/phone_display.rb
@@ -3,43 +3,6 @@ require 'tty-box'
 module Hangman
   module Display
     module PhoneDisplay
-      SAVED_GAMES = YAML.safe_load(
-        File.read('saved_games.yaml'),
-        permitted_classes: [Board, Game, HumanPlayer, ComputerPlayer, Symbol, Time]
-      ) || {}
-
-      def self.list_saved_games
-        saved_games_list = ''
-        SAVED_GAMES.keys.each_with_index { |saved_game, i| saved_games_list << i.to_s << '. ' << saved_game << "\n\n" }
-        TTY::Box.frame(
-          saved_games_list,
-          padding: [1, 1],
-          title: { top_center: ' Choose a game : ' }
-        )
-        saved_games_list
-      end
-
-      def self.saved_game_to_load
-        saved_games_list = list_saved_games
-        loop do
-          index = gets.chomp
-
-          if index.match?(/^#{[*1..SAVED_GAMES.keys.length - 1]}$/)
-            chosen_saved_game_name = saved_games_list.split("\n\n").filter do |saved_game|
-              saved_game.start_with?(index)
-            end
-            chosen_saved_game_name = chosen_saved_game_name.split(' ', 2)[1]
-            break SAVED_GAMES[chosen_saved_game_name.to_sym]
-          end
-
-          invalid_index
-        end
-      end
-
-      def self.invalid_index
-        puts "Please enter a number from 1 to #{SAVED_GAMES.keys.length - 1}"
-      end
-
       def clarify_rules
         show_guide
         loop do
@@ -49,17 +12,6 @@ module Hangman
           break if input.empty?
         end
         Display.clear
-      end
-
-      def human_player_role
-        puts
-        ask_for_human_player_role
-        loop do
-          print 'Enter either the number 1 or 2 : '
-          role = gets.chomp
-          puts
-          break Display.clear || role if valid_role?(role)
-        end
       end
 
       def ask_for_human_player_role
