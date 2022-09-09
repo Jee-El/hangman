115940a8d213e3c9fcf3f2c98000d3b4e529ffb7
jee-el <98971420+Jee-El@users.noreply.github.com>
2022-09-09 03:00:30 +0100

    Nest PlatformName into Display

--- lib/platform_name.rb
+++ lib/platform_name.rb
@@ -3,25 +3,27 @@
 require 'tty-box'
 
 module Hangman
-  module PlatformName
-    def self.get
-      platform_names_by_numbers = { '1' => 'phone', '2' => 'computer' }
-      ask
-      loop do
-        name = gets.chomp
-        break platform_names_by_numbers[name] if name.strip.match?(/^(1|2)$/)
+  module Display
+    module PlatformName
+      def self.get
+        platform_names_by_numbers = { '1' => 'phone', '2' => 'computer' }
+        ask
+        loop do
+          name = gets.chomp
+          break platform_names_by_numbers[name] if name.strip.match?(/^[12]$/)
 
-        print 'Enter either the number 1 or 2 : '
+          print 'Enter either the number 1 or 2 : '
+        end
       end
-    end
 
-    def self.ask
-      puts
-      puts TTY::Box.frame(
-        "1 -> Phone\n\n2 -> Computer",
-        padding: [1, 1],
-        title: { top_center: ' What device are you on? ' }
-      )
+      def self.ask
+        puts
+        puts TTY::Box.frame(
+          "1 -> Phone\n\n2 -> Computer",
+          padding: [1, 1],
+          title: { top_center: ' What device are you on? ' }
+        )
+      end
     end
   end
 end
