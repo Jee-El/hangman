ae0d8ff8c20d9592059ee20dbde08b3361a84452
jee-el <98971420+Jee-El@users.noreply.github.com>
2022-09-09 23:31:42 +0100

    Improve board's look when it's displaying errors

--- lib/board.rb
+++ lib/board.rb
@@ -3,10 +3,12 @@ require 'colorize'
 module Hangman
   # Draws the underscores & letters
   class Board
-    def initialize(word_length)
+    attr_accessor :left_guesses
+
+    def initialize(word_length, max_guesses)
       @slots = Array.new(word_length) { '_' }
       @wrong_guesses = []
-      @guesses_count = 0
+      @left_guesses = max_guesses
     end
 
     def slots
@@ -17,15 +19,23 @@ module Hangman
       @wrong_guesses.join(' ')
     end
 
+    def draw(guess, word)
+      update(guess, word) unless guess.nil? || guess.length > 1
+
+      [slots, wrong_guesses].each { |elem| print(elem) || print(' | ') }
+      print(@left_guesses) || 2.times { puts }
+
+      # Invalid guess message
+      print guess if guess.length > 1
+    end
+
     def update(guess, word)
-      @guesses_count += 1
       if word.include?(guess)
         word.length.times { |i| @slots[i] = guess.green if word[i] == guess }
       else
         @wrong_guesses.push(guess.red)
+        @left_guesses -= 1
       end
-      [slots, wrong_guesses].each { |elem| print(elem) || print(' | ') }
-      print @guesses_count || puts
     end
   end
 end
